72a4246f481b516d3a06c96b0ad1bb5eaef6aa76
ShixiangWang <wangshx@shanghaitech.edu.cn>
2019-05-16 20:12:34 +0800

    add test data & update prepare

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # neopip - a neoantigen prediction workflow
 
-Tools: snakemake + docker + singularity + conda
+Tools: snakemake + docker + singularity + conda?
 
 reference: https://github.com/ShixiangWang/Variants2Neoantigen
 
@@ -13,7 +13,29 @@ Following tools are needed before using **neopip**:
 - gawk
 - wget
 
-```
-"This package installs only the variant effect predictor (VEP) library\ncode. To install data libraries, you can use the 'vep_install' command\ninstalled along with it. For example, to install the VEP library for human\nGRCh38 to a directory\n\nvep_install -a cf -s homo_sapiens -y GRCh38 -c /output/path/to/GRCh38/vep --CONVERT\n\n(note that vep_install is renamed from INSTALL.pl\n to avoid having generic script names in the PATH)\n\nThe --CONVERT flag is not required but improves lookup speeds during\nruns. See the VEP documentation for more details\n\nhttp://www.ensembl.org/info/docs/tools/vep/script/vep_cache.html\n"                                                                                                                         | b"This package installs only the variant effect predictor (VEP) library\ncode. To install data libraries, you can use the 'vep_install.pl' command\ninstalled along with it. For example, to install the VEP library for human\nhg19/GRCh37 to a directory:\n\nvep_install.pl -a cf -s homo_sapiens -y GRCh37 -c /output/path/to/hg19/vep\nvep_convert_cache.pl -species homo_sapiens -version 86_GRCh37 -d /output/path/to/hg19/vep\n\n(note that vep_install.pl is renamed from INSTALL.pl\n and vep_convert_cache.pl from covert_cache.pl\n to avoid having generic script names in the PATH)\n\nThe convert cache step is not required but improves lookup speeds during\nruns. See the VEP documentation for more details:\n\nhttp://useast.ensembl.org/info/docs/tools/vep/script/vep_cache.html\n"                                                                                                                                                       done
+After prepare neoantigen predicton softwares, download necessary data libraries.
 
+```
+This package installs only the variant effect predictor (VEP) library code. 
+To install data libraries, you can use the 'vep_install' command installed along with it.
+
+For example, to install the VEP library for human GRCh38 to a directory
+
+    vep_install -a cf -s homo_sapiens -y GRCh38 -c /output/path/to/GRCh38/vep --CONVERT
+
+To install the VEP library for human hg19/GRCh37 to a directory:
+
+    vep_install.pl -a cf -s homo_sapiens -y GRCh37 -c /output/path/to/hg19/vep
+    vep_convert_cache.pl -species homo_sapiens -version 86_GRCh37 -d /output/path/to/hg19/vep
+
+(note that vep_install is renamed from INSTALL.pl to avoid having generic script names in the PATH)
+The --CONVERT flag is not required but improves lookup speeds during runs. 
+
+See the VEP documentation for more details\n\nhttp://www.ensembl.org/info/docs/tools/vep/script/vep_cache.html\n"                                                                 
+```
+
+Use 
+
+```
+vep_install -a cf -s homo_sapiens -y GRCh37 -c $HOME/.vep –CONVERT
 ```
--- prepare.py
+++ prepare.py
@@ -38,35 +38,51 @@ def main(neopip_loc="%s/.neopip" %home, miniconda_loc="%s/.neopip/miniconda" %ho
     # Set logging level and format
     datefmt = '%Y/%m/%d %H:%M:%S'
     logfmt = '[%(asctime)s - %(levelname)s - %(filename)s]: %(message)s'
-    logging.basicConfig(level=logging.INFO, 
+    logging.basicConfig(level=logging.DEBUG, 
                         filename=logfile,
                         datefmt=datefmt,
                         format=logfmt)
     logger = logging.getLogger(__name__)
     stream_handler = logging.StreamHandler(sys.stdout)
-    stream_handler.setLevel(level=logging.DEBUG)
-    stream_handler.setFormatter(logging.Formatter(fmt=logfmt, datefmt=datefmt))
+    stream_handler.setLevel(level=logging.INFO)
+    #stream_handler.setFormatter(logging.Formatter(fmt=logfmt, datefmt=datefmt))
     logger.addHandler(stream_handler)
 
+    # Preface
+    logger.info("========================================================================")
+    logger.info("Prepare conda environments, softwares and data for neoantigen prediction")
+    logger.info("========================================================================")
+    logger.info("Author  : %s"%__author__)
+    logger.info("Email   : %s"%__email__)
+    logger.info("Version : %s"%__prediction_version__)
+    logger.info("========================================================================")
+    logger.info("Except VEP data is store at ~/.vep,")
+    logger.info("    all others will put into %s"%neopip_loc)
+    logger.info("Custom conda environment path is %s"%miniconda_loc)
+    logger.info("    neoda is a wrapper for better manage conda environments here")
+    logger.info("")
+    logger.info("Prepare process is starting, log info will output to %s", logfile)
+    logger.info("")
+    logger.info("Enjoy it!")
+    logger.info("========================================================================")
+    
     # Create base directory
-    logger.info("Create %s", neopip_loc)
     try:
         execute(["mkdir", "-p", neopip_loc])
     except Exception:
         logger.error("Failed to create directory %s", neopip_loc, exc_info=True)
         sys.exit()
 
-    logger.info("Prepare process is starting, log info will output to %s", logfile)
     
     # Install miniconda
-    logger.info("Download miniconda to /tmp")
+    logger.info("> Download miniconda to /tmp")
     try:
         execute(["wget", "-c", "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh", "-O", "/tmp/miniconda.sh"])
     except Exception:
         logger.error("Failed to download miniconda, please check your network", exc_info=True)
         sys.exit()
 
-    logger.info("Start Installing miniconda to %s", miniconda_loc)
+    logger.info("> Start Installing miniconda to %s", miniconda_loc)
     try:
         execute(["sh",  "/tmp/miniconda.sh", "-b", "-p", miniconda_loc])
     except Exception:
@@ -78,16 +94,12 @@ def main(neopip_loc="%s/.neopip" %home, miniconda_loc="%s/.neopip/miniconda" %ho
 
     # Create conda environments for python 2.7 and
     # install softwares based on python 2.7
-    logger.info("Create python 2.7 environment %s", envs_py27.env_location)
+    logger.info("> Create python 2.7 environment %s", envs_py27.env_location)
     try:
         execute([envs_py27.conda, "create", "-p", envs_py27.env_location, "python=2.7", "biopython", "-y"])
     except Exception:
         logger.error("Fail to create the environment", exc_info=True)
         sys.exit()
-
-    # Install vep and vcf2maf on python 3
-    cmds = [envs_py3.activate_cmd,  "%s install -c bioconda ensembl-vep vcf2maf -y"%envs_py3.conda, envs_py3.deactivate_cmd]
-    execute(cmds, sep = " && ")
    
    # Copy data ExAC_nonTCGA.r0.3.1.sites.vep.vcf.gz and iedb
    # See:
@@ -96,41 +108,48 @@ def main(neopip_loc="%s/.neopip" %home, miniconda_loc="%s/.neopip/miniconda" %ho
    
     data_dir = "%s/data" % neopip_loc
     if not os.path.isdir(data_dir):
-        logger.info("Data directory %s does not exist, create it" %data_dir)
         os.system("mkdir -p %s" % data_dir)
     
-    logger.info("Copy data files from neopip to %s data subdirectory"%neopip_loc)
+    logger.info("> Copy data files from neopip to %s data subdirectory"%neopip_loc)
     os.system("cp -r %s/* %s" %(os.path.join(this_dir, "data"), data_dir))
+
+    # Install vep & vcf2maf on python 3 environment
+    # https://bioconda.github.io/recipes/ensembl-vep/README.html
+    logger.info("> Install ensembl-vep vcf2maf bcftools")
+    cmds = [envs_py3.activate_cmd,  "%s install -c bioconda ensembl-vep vcf2maf bcftools -y"%envs_py3.conda, envs_py3.deactivate_cmd]
+    execute(cmds, sep = " && ")
+    logger.info(">>> Copy ExAC_nonTCGA.r0.3.1.sites.vep.vcf.gz* to ~/.vep")
+    os.system("mkdir ~/.vep && cp %s/ExAC_nonTCGA.r0.3.1.sites.vep.vcf.gz* ~/.vep"%data_dir)
 
     # IEDB softwares
     iedb_dir = "%s/iedb" % neopip_loc
     if not os.path.isdir(iedb_dir):
-        logger.info("IEDB directory %s does not exist, create it" %iedb_dir)
         os.system("mkdir -p %s" % iedb_dir)
     # IEDB MHC I 
-    logger.info("Download and install IEDB MHC I  %s" %__mhc_i_version__)
+    logger.info("> Download and install IEDB MHC I  %s" %__mhc_i_version__)
     cmds = [envs_py27.activate_cmd, "wget -c https://downloads.iedb.org/tools/mhci/{0}/IEDB_MHC_I-{0}.tar.gz -O /tmp/IEDB_MHC_I.tar.gz".format(__mhc_i_version__),
             "tar zxf /tmp/IEDB_MHC_I.tar.gz -C %s"%iedb_dir, "cd %s/mhc_i"%iedb_dir, "./configure", envs_py27.deactivate_cmd]
     execute(cmds, sep = " && ")
 
     # IEDB MHC II 
     # QA: Perl script can't locate Env.pm in @INC - yum install perl-Env
-    logger.info("Download and install IEDB MHC II %s" %__mhc_ii_version__)
+    logger.info("> Download and install IEDB MHC II %s" %__mhc_ii_version__)
     cmds = [envs_py27.activate_cmd, "wget -c https://downloads.iedb.org/tools/mhcii/{0}/IEDB_MHC_II-{0}.tar.gz -O /tmp/IEDB_MHC_II.tar.gz".format(__mhc_ii_version__),
             "tar zxf /tmp/IEDB_MHC_II.tar.gz -C %s"%iedb_dir, "cd %s/mhc_ii"%iedb_dir, "python ./configure.py", envs_py27.deactivate_cmd]
     execute(cmds, sep = " && ")
 
     
     # Install pvactools
-    logger.info("Install pvactools %s"%__pvactools_version__)
+    logger.info("> Install pvactools %s"%__pvactools_version__)
     cmds = [envs_py3.activate_cmd, "%s install tensorflow=1.5.0 -y"%envs_py3.conda, envs_py3.deactivate_cmd]
     execute(cmds, sep = " && ")
     cmds = [envs_py3.activate_cmd, "pip install pvactools==%s"%__pvactools_version__, envs_py3.deactivate_cmd]
     execute(cmds, sep = " && ")
 
+    
     mhcflurry_dir = "%s/data/mhcflurry_data" %neopip_loc
+    logger.info(">>> Download mhcflurry data to %s" %mhcflurry_dir)
     if not os.path.isdir(mhcflurry_dir):
-        logger.info("mhcflurry data directory %s does not exist, create it" %mhcflurry_dir)
         os.system("mkdir -p %s" % mhcflurry_dir)
 
     # Set data of mhcflurry to custom directory (is this good for analysis?)
@@ -138,11 +157,11 @@ def main(neopip_loc="%s/.neopip" %home, miniconda_loc="%s/.neopip/miniconda" %ho
             "export MHCFLURRY_DATA_DIR=%s"%mhcflurry_dir, "mhcflurry-downloads fetch", envs_py3.deactivate_cmd]
     execute(cmds, sep = " && ")
 
-    logger.info("Download pvacseq example data to %s" %neopip_loc)
-    cmds = [envs_py3.activate_cmd, "pvacseq download_example_data %s/data"%neopip_loc , envs_py3.deactivate_cmd]
+    logger.info(">>> Download pvacseq example data to %s" %data_dir)
+    cmds = [envs_py3.activate_cmd, "pvacseq download_example_data %s"%data_dir , envs_py3.deactivate_cmd]
     execute(cmds, sep = " && ")
     
-    logger.info("Neoantigen prediction prepare process finished!")
+    logger.info("> Neoantigen prediction prepare process finished!")
 
 
 if __name__ == "__main__":
